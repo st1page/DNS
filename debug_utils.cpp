00b959487ebb669657ad40567787e2ee289544a7
stonepage <1245835950@qq.com>
2018-12-23 15:37:44 +0800

    dn2msg fin

--- debug_utils.cpp
+++ debug_utils.cpp
@@ -21,6 +21,16 @@ void test_isDN(){
 	s = "www.a1234567b1234567c1234567d1234567e1234567f1234567g1234567h1234567.com";
 	printf("%s %d\n",s,isDomainName(s));
 }
+void test_dn2msg(){
+	char *s, *t;
+	s = "www.google.com";
+	t = dn2msg(s);
+	printf("%s#\n%s#\n\n",s,t);
+	s = "www.google.com.";
+	t = dn2msg(s);
+	printf("%s#\n%s#\n\n",s,t);
+}
 int main(){
 	test_isDN();
+	test_dn2msg();
 }
--- utils.hpp
+++ utils.hpp
@@ -2,8 +2,10 @@
 #define _UTILS_HPP_
 
 #include <string.h>
+#include <stdlib.h>
 
-bool isDomainName(const char *dn){
+#include "stdio.h"
+bool isDomainName(char *dn){
 	size_t len = strlen(dn);
 	if(len == 0 || len > 255 ||
 		dn[0]=='.' || dn[0]=='-' ||
@@ -15,5 +17,20 @@ bool isDomainName(const char *dn){
 		if(r-l == 0 || r-l > 63) return false;
 	}
 	return true;
+}
+char* dn2msg(char *dn){
+	puts("aaa");
+	char *msg;
+	size_t len = strlen(dn);
+	if(dn[len-1]!='.') msg = (char*)calloc(len+2,1);
+	else msg = (char*)calloc(len+1,1);
+	size_t l,r;
+	for(size_t i=0;i<len;i=r+1){
+		l = i; r = i;
+		while(dn[r]!='.'&&r!=len) r++;
+		msg[l] = r-l;
+		for(size_t j=l;j<r;j++) msg[j+1] = dn[j];
+	}
+	return msg;
 }
 #endif
